6150bf56d39be144537191be95531284de1f11e0
Jakob <jhoefker@th-koeln.de>
2018-05-18 15:54:48 +0200

    Ein Versuch. Hinzufügen von Testklasse mit Funktion, die etwas ausgibt

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/jhoefker/borgdir-media/app/controller/test"
 	"github.com/jhoefker/borgdir-media/app/route"
 )
 
 func main() {
 	route.MapToController()
+	test.Funktion1()
 	log.Println("Listening on Port 3000 ...")
 	http.ListenAndServe(":3000", nil)
 }
